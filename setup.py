e731474c1d784129b95c827a61c5a7ceee7e8699
Benjamin Schnabel <bsesic@users.noreply.github.com>
2024-02-20 14:08:44 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,4 +4,4 @@ from setuptools import setup, find_packages
 setup(name='tanhuma',
       version='0.1',
       packages=find_packages(),
-      scripts=['portal/manage.py'])
+      scripts=['manage.py'])
